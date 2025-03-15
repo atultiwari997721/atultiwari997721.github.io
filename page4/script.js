bbb394f20af9ee316ee8ed6ffc2965afde2cf11d
atultiwari997721 <atultiwari997721@gmail.com>
2025-03-15 22:58:59 +0530

    adding login page

---
+++ page4/script.js
@@ -0,0 +1,26 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var message = document.getElementById('message');
+
+    // Simple validation
+    if (username === 'user' && password === 'pass') {
+        message.textContent = 'Login successful!';
+        message.style.color = 'green';
+        window.location.href = 'https://www.google.com'; // Redirect to Google
+    } else {
+        message.textContent = 'Invalid username or password';
+        message.style.color = 'red';
+    }
+    if (username ==='atul' && password === 'atul') {
+        message.textContent = 'Login successful!';
+        message.style.color = 'green';
+        window.location.href = 'https://www.youtube.com'; // Redirect to Google
+    } else {
+        message.textContent = 'Invalid username or password';
+        message.style.color = 'red';
+        
+    }
+});
